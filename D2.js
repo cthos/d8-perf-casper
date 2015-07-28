15a7ac0c5cd4f4892438677e891356ccc792839c
cthos <daginus@gmail.com>
2015-07-28 12:56:36 -0700

    update shell script and get d2 running a drush cr.

--- D2.js
+++ D2.js
@@ -15,9 +15,21 @@ var login = function () {
 
 casper.start(url_base, login);
 
+casper.then(function () {
+  // TODO: Is there a better way to do this?
+  var childProcess = require("child_process");
+  var casp = this;
+  childProcess.execFile('drush', [casper.cli.get('alias'), 'cr'], null, function (err, stdout, stderr) {
+    require('utils').dump(stdout);
+    require('utils').dump(stderr);
+  });
+}).wait(10000); // Wait a bit for the drush command to complete.
+
 casper.thenOpen(url_base, function () {});
 casper.thenOpen(url_base, function () {});
 casper.thenOpen(url_base + '/node/1', function () {});
-casper.thenOpen(url_base, function () {});
+casper.thenOpen(url_base + '/node/1', function () {});
+casper.thenOpen(url_base + '/node/2', function () {});
+casper.thenOpen(url_base + '/node/2', function () {});
 
 casper.run();
