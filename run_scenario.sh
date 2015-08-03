13e82906f0c168df125ed4072bedf40c90684231
cthos <daginus@gmail.com>
2015-08-02 19:46:09 -0700

    Hard-code admin password.

--- run_scenario.sh
+++ run_scenario.sh
@@ -14,25 +14,23 @@ case $1 in
   A)
     echo "Running site-install on $sitealias"
     ## Maybe sed would be better here.
-    pass=`drush $sitealias site-install $profile -y | grep password | cut -d ' ' -f 10`
-    echo "The password for admin is $pass"
-    casperjs A.js $pass --uri=$siteuri
+    drush $sitealias site-install $profile -y --account-pass=admintest
+    casperjs A.js "admintest" --uri=$siteuri
     ;;
   B)
-    drush $sitealias site-install $profile -y
+    drush $sitealias site-install $profile -y --account-pass=admintest
     drush $sitealias pm-uninstall page_cache -y
     casperjs B.js --uri=$siteuri
     ;;
   C)
-    drush $sitealias site-install $profile -y
+    drush $sitealias site-install $profile -y --account-pass=admintest
     casperjs C.js --uri=$siteuri
     ;;
   D)
     echo "Running site-install on $sitealias"
     ## Maybe sed would be better here.
-    pass=`drush $sitealias site-install $profile -y | grep password | cut -d ' ' -f 10`
-    echo "The password for admin is $pass"
-    casperjs D.js $pass
+    drush $sitealias site-install $profile -y --account-pass=admintest
+    casperjs D.js "admintest"
     casperjs D2.js --alias=@drupalvm.dev --uri=$siteuri
     ;;
 esac
